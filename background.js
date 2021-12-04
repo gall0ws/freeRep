2b02ae7747b319b1cd565ea73cc7f7193f67fa68
gall0ws <gall0ws@aleph0.pw>
2021-12-04 15:55:37 +0100

    added background.js to edit request headers for repubblica.it

---
+++ background.js
@@ -0,0 +1,25 @@
+const fixHeaders = ({requestHeaders}) => {
+    requestHeaders = requestHeaders.filter(h => ! ["referer", "cookie"].includes(h.name.toLowerCase()))
+    requestHeaders.push({
+        name: "Referer",
+        value: "https://m.google.it/"
+    })
+    return {requestHeaders}
+}
+
+const filters = {
+    urls: [ "https://*.repubblica.it/*" ],
+    types: [ "main_frame" ]
+}
+const opts = [ "blocking", "requestHeaders" ]
+
+try {
+    void browser
+} catch (e) {
+    if (e instanceof ReferenceError && typeof chrome != "undefined") {
+        browser = chrome
+        opts.push("extraHeaders")
+    }
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(fixHeaders, filters, opts)
